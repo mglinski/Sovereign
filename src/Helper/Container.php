c5a93dec123e8e88171be63e940de38575d8e413
Michael Karbowiak <karbowiak@gmail.com>
2016-06-01 16:22:22 +0200

    Move the init of the container into the container function itself, since pthreads is being a bitch

--- src/Helper/Container.php
+++ src/Helper/Container.php
@@ -14,6 +14,17 @@ if (! function_exists('getContainer')) {
             $container->delegate(
                 new ReflectionContainer
             );
+
+
+            // register default config file
+            $container->add('configFile', BASEDIR . '/config/config.php');
+
+            // Add the default system service provider
+            $container->addServiceProvider(\Sovereign\Service\SystemServiceProvider::class);
+
+            // Load the bot into the service container
+            $container->share('app', \Sovereign\Sovereign::class)->withArgument($container);
+
         }
 
         return $container;
